2a08baafafb671525413496dc68a2875b171075d
souppower <souppower@goodpatch.com>
2018-02-03 17:29:51 +0900

    :up: add webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,23 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./counter.js",
+  entry: "./src/main.js",
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist")
   },
   module: {
     rules: [{ test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html"
+    })
+  ],
+  devServer: {
+    historyApiFallback: true,
+    stats: "minimal",
+    port: 3000
   }
 };
