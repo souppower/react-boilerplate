0b1b9503c3d9dc11031a3fa93c85b9b6b21c9ece
souppower <souppower@goodpatch.com>
2018-02-03 15:49:08 +0900

    :up: add webpack

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./counter.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  module: {
+    rules: [{ test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }]
+  }
+};
