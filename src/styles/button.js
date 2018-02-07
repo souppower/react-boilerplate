337c842a7836d7ff7c7dc631edc311230c21fcfe
souppower <souppower@goodpatch.com>
2018-02-08 00:13:42 +0900

    :recycle: resolve flowtype error

--- src/styles/button.js
+++ src/styles/button.js
@@ -1,16 +1,16 @@
-import styled from 'styled-components';
+import styled from 'styled-components'
 
 export const BaseButton = styled.button`
   border-radius: 5px;
   border: none;
   color: white;
   padding: 15px;
-`;
+`
 
 export const BlueButton = BaseButton.extend`
   background: cornflowerblue;
-`;
+`
 
 export const RedButton = BaseButton.extend`
   background: orangered;
-`;
+`
