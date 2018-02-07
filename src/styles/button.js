ef25dd428bafaa540678d978747b705ea2bb9cc3
souppower <souppower@goodpatch.com>
2018-02-08 00:04:00 +0900

    :up: add stylelint

--- src/styles/button.js
+++ src/styles/button.js
@@ -1,28 +1,16 @@
 import styled from 'styled-components';
 
-// prettier-ignore
-// language=SCSS
 export const BaseButton = styled.button`
-  & {
-    border-radius: 5px;
-    border: none;
-    color: white;
-    padding: 15px;
-  }
+  border-radius: 5px;
+  border: none;
+  color: white;
+  padding: 15px;
 `;
 
-// prettier-ignore
-// language=SCSS
 export const BlueButton = BaseButton.extend`
-  & {
-    background: cornflowerblue;
-  }
+  background: cornflowerblue;
 `;
 
-// prettier-ignore
-// language=SCSS
 export const RedButton = BaseButton.extend`
-  & {
-    background: orangered;
-  }
+  background: orangered;
 `;
